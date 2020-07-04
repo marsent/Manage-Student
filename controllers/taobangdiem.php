0abb08de9b844140c451f9104ea11cbef36a726d
Tuấn Anh <tuananh8423@gmail.com>
2020-07-04 16:34:15 +0700

    refix

--- controllers/taobangdiem.php
+++ controllers/taobangdiem.php
@@ -6,22 +6,22 @@ $error = [];
 
 $namhoc = $_POST['namhoc'];
 $hocky = $_POST['hocky'];
-$lop = $_POST['lop'];
-$monhoc = $_POST['monhoc'];
+$lop =$_POST['lop'];
+$monhoc =$_POST['monhoc'];
 $mahocky=getMaHK($conn,$hocky,$namhoc);
-// $sql = "SELECT * from quatrinhhoc join hocky
-// on quatrinhhoc.mahocky=hocky.mahocky
-// join bangdiemmon on bangdiemmon.maqthoc=quatrinhhoc.maqthoc
-// where  malop='$lop' and hocky='$hocky' and manam='$namhoc' and mamonhoc='$monhoc' and mamonhoc is null";
-// $result=$conn->query($sql);
-// if($result->num_rows!=0){
-//     foreach($result as $value){
-//         $maQTH = $value['MaQTHoc'];
-//         $maHS = $value['MaHocSinh'];
-//         $sql = "INSERT INTO bangdiemmon (MaBangDiemMon,MaQTHoc,MaMonHoc) VALUES('" . $maQTH . $monhoc . "','$maQTH','$monhoc')";
-//         $conn->query($sql);
-//     }
-// }
+$sql = "SELECT * from quatrinhhoc join hocky
+on quatrinhhoc.mahocky=hocky.mahocky
+join bangdiemmon on bangdiemmon.maqthoc=quatrinhhoc.maqthoc
+where  malop='$lop' and hocky='$hocky' and manam='$namhoc' and mamonhoc='$monhoc' and mamonhoc is null";
+$result=$conn->query($sql);
+if($result->num_rows!=0){
+    foreach($result as $value){
+        $maQTH = $value['MaQTHoc'];
+        $maHS = $value['MaHocSinh'];
+        $sql = "INSERT INTO bangdiemmon (MaBangDiemMon,MaQTHoc,MaMonHoc) VALUES('" . $maQTH . $monhoc . "','$maQTH','$monhoc')";
+        $conn->query($sql);
+    }
+}
 
 $sql = "select * from quatrinhhoc join hocsinh
 on quatrinhhoc.mahocsinh=hocsinh.mahocsinh
@@ -54,5 +54,15 @@ on quatrinhhoc.mahocky=hocky.mahocky WHERE MaLop='$lop' AND MaNam='$namhoc' and 
     );
 }
 
+$sql="SELECT quatrinhhoc.* from quatrinhhoc left JOIN bangdiemmon on quatrinhhoc.MaQTHoc=bangdiemmon.MaQTHoc WHERE mahocky='$mahocky' and malop='$lop' and mabangdiemmon is null";
+$result=$conn->query($sql);
+if($result->num_rows!=0){
+    foreach($result as $value){
+        $maQTH =$value['MaQTHoc'];
+        $maHS = $value['MaHocSinh'];
+        $sql = "INSERT INTO bangdiemmon (MaBangDiemMon,MaQTHoc,MaMonHoc) VALUES('" . $maQTH . $monhoc . "','$maQTH','$monhoc')";
+        $conn->query($sql);
+    }
+}
 echo json_encode($error);
 $conn->close();
