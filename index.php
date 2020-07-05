0e1bfb0a5e07e7f02dc07c46881ff6d0fd34ef6d
havuongquoc2000@gmail.com <havuongquoc2000@gmail.com>
2020-07-05 08:25:17 +0700

    add Homepage

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php include 'View/element/header.php';
+?>
+<style>
+    body,
+    html {
+        height: 100%;
+    }
+
+    .bg {
+        /* The image used */
+        background-image: url("https://www.isd518.net/wp-content/uploads/2020/01/isd518-highschool.jpg");
+
+        /* Full height */
+        height: 100%;
+
+        /* Center and scale the image nicely */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .main{
+        height: 100%;
+    }
+    .navbar.navbar-expand-lg{
+        margin-bottom: 0 !important; 
+    }
+    h2{
+        text-align: right;
+        font-size: 4rem;
+        color : #ffc300; 
+    }
+</style>
+<div class="main">
+    <div class="bg">
+    <h2>Phần Mềm Quản Lí Học Sinh</h2>
+    </div>
+</div>
+
+
+<?php include 'View/element/footer.php'; ?>
